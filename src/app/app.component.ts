cb53d1a890c6f58e67cbcb3f9402b0acd74baff3
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-05-25 12:23:29 +0300

    Added page prop, and passed to Flex and Grid components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { PageType } from './models/page';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  page: PageType = PageType.Flex;
   title = 'angular-template-areas';
+
+  changePage() {
+    if (this.page === PageType.Flex) this.page = PageType.Grid;
+    else this.page = PageType.Flex;
+  }
 }
