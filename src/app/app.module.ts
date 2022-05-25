2ce0774628d2da1dee66c0392f93b67f0969d073
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-05-25 11:19:46 +0300

    Added Grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexComponent } from './components/flex/flex.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlexComponent
+    FlexComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
