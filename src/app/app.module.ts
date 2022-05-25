7d45c19c789ccd93d1950c519fae1544d6245e95
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-05-25 11:19:16 +0300

    Added Flex component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexComponent } from './components/flex/flex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlexComponent
   ],
   imports: [
     BrowserModule,
